0fd72978cb4fbb1232c2d4f74da235e192921266
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-07-06 08:38:19 +0000

    Upload New File

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+clear
+
+echo "################### Build & deploy Gitlab-CE ###################"
+docker compose -p gitlab-ce up -d --build --wait gitlab-ce
+echo "###############################################################"
+
+echo "..."
+
+echo "#################### Check Gitlab-CE state ####################"
+docker compose -p gitlab-ce ps
+echo "###############################################################"
+
+echo "..."
+
+echo "############## Gitlab runner registration token ###############"
+RUNNER_REG_TOKEN=$(docker exec -it gitlab-ce gitlab-rails runner -e production 'puts Gitlab::CurrentSettings.current_application_settings.runners_registration_token')
+echo "Token: $RUNNER_REG_TOKEN"
+echo "###############################################################"
+
+echo "..."
+
+echo "################ Gitlab initial root password #################"
+docker exec -it gitlab-ce bash -c 'grep 'Password:' /etc/gitlab/initial_root_password 2>/dev/null || echo "Custom root password already exists."'
+echo "###############################################################"
+
+echo "..."
+
+echo "################# Base runners registration ###################"
+echo "Registering  gitlab-build-runner ..."
+./reg_new_runner.sh gitlab-build-runner build $RUNNER_REG_TOKEN
+echo "Registering  gitlab-test-runner ..."
+./reg_new_runner.sh gitlab-test-runner test $RUNNER_REG_TOKEN
+echo "Registering  gitlab-deploy-runner ..."
+./reg_new_runner.sh gitlab-deploy-runner deploy $RUNNER_REG_TOKEN
+echo "###############################################################"
