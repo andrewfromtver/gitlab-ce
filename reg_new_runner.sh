7d49eda6d208091b65c4bbd1b53fd85b3aff4274
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-07-06 08:39:03 +0000

    Upload New File

---
+++ reg_new_runner.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+RUNNER_CONTAINER_ID=$1
+RUNNER_TAG=$2
+GITLAB_RUNNER_REG_TOKEN=$3
+
+GITLAB_SUBNET=$(docker network ls | grep gitlab_ce | cut -d " " -f 4)
+
+echo "Registering ...";
+
+docker exec -it $RUNNER_CONTAINER_ID bash -c 'gitlab-runner register \
+  --non-interactive \
+  --executor "shell" \
+  --docker-image alpine:latest \
+  --url "http://gitlab-ce.local/" \
+  --registration-token "'$GITLAB_RUNNER_REG_TOKEN'" \
+  --description "docker-'$RUNNER_TAG'-runner" \
+  --maintenance-note "Docker runner for gitlab-ce" \
+  --tag-list "docker,'$RUNNER_TAG'" \
+  --run-untagged="true" \
+  --locked="false" \
+  --access-level="not_protected" > /dev/null 2>&1; \
+  echo "    network_mode = \"'$GITLAB_SUBNET'\"" >> /etc/gitlab-runner/config.toml'; 
+
+echo "Done"
